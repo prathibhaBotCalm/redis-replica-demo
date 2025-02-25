3d214ec3ed7ea07268f2fbd57c1fb5c31634fbc4
prathibhaBotCalm <prathibha.r@botcalm.com>
2025-02-25 10:13:21 +0530

    sample commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { getUsers } from "@/actions/user";
 
 export default async function Home() {
-  const users = (await getUsers()) || [];
+  const users = await getUsers();
   console.log(users);
   return <div>{users?.map((user) => <li>user?.name</li>)}</div>;
 }
